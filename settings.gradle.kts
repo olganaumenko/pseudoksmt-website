bfccf8d68b96482bce3c72a4bbc583df97a67225
Olga Naumenko <64418523+olganaumenko@users.noreply.github.com>
2023-06-20 11:41:12 +0300

    Minor fixes

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+rootProject.name = "ksmt"
+include("ksmt-core")
+include("ksmt-z3")
+include("ksmt-bitwuzla")
+include("ksmt-yices")
+include("ksmt-runner")
+include("ksmt-runner:solver-generator")
+include("ksmt-test")
+include("ksmt-cvc5")
+
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.name == "rdgen") {
+                useModule("com.jetbrains.rd:rd-gen:${requested.version}")
+            }
+        }
+    }
+}
